22cf57a84ea134cb0243d63db4a2c2f82167cf18
StefAranzazu <stefanyaranzazu1310@gmail.com>
2022-02-19 15:08:06 +0000

    todo list fetch

---
+++ src/js/component/DeleteAllBut.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const DeleteAllBut = () => {
+	return (
+		<div className="d-flex justify-content-center finalbut">
+			<button type="button" className="btn btn-danger DelAll">
+				Delete All!
+			</button>
+		</div>
+	);
+};
+export default DeleteAllBut;
---
+++ src/js/component/NothingToDel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NothingToDel = () => {
+	return (
+		<div className="d-flex justify-content-center finalbut">
+			<button type="button" className="btn btn-secondary Empty" disabled>
+				{" "}
+				Nothing To Do!
+			</button>
+		</div>
+	);
+};
+export default NothingToDel;
